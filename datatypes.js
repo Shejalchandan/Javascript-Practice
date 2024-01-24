e3c34956d2da21d6f8b5f0f773eaf71cde76e76e
Shejal Chandan <138048164+Shejalchandan@users.noreply.github.com>
2024-01-24 23:23:17 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+//treat all JS code as newer version
+
+// alert(9+9)  we are using nodejs not browser
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+let state = null
+let h;
+
+// datatypes in Java Scipt
+/*
+1. number 
+2. bigInt
+3. string => "" or''
+4. boolean => true/false
+5. null => standalone value
+6. undefined 
+7. symbol => unique
+
+Object
+*/
+console.log(typeof null);
+console.log(typeof undefined);
