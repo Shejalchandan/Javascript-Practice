41eef4bf3f2caf8c79db092a343b1805324d7c40
Shejal Chandan <138048164+Shejalchandan@users.noreply.github.com>
2024-01-25 23:29:51 +0530

    Add files via upload

---
+++ comparision.js
@@ -0,0 +1,36 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log("---------------------");
+
+
+
+console.log("2">1) ;
+console.log("01">90);
+console.log("++++++++++++++++");
+
+//avoid thsi type od conversion
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log("*******************");
+//avoid
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log("&&&&&&&&&&&&&&&&");
+
+// ====
+console.log("2" === 2);  // datatype check krta he same he ya nhi
+console.log("2"==2);
+
+
+
+
+
+
+
+
