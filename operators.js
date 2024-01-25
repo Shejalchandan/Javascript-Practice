ff720d43b8968fb91d59bd6246156a3516c56b55
Shejal Chandan <138048164+Shejalchandan@users.noreply.github.com>
2024-01-25 23:30:31 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,31 @@
+let value = 9
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+ let str1="hello"
+ let str2="Shejal"
+ console.log(str1+str2)
+ let str3 = str1+str2;
+ console.log(str3);
+
+ console.log("1"+2); //12
+ console.log(1+ "2"); //12
+ console.log("1" + 2 + 2); //122
+ console.log(1 + 2 + "3"); //33
+
+ console.log(+true);
+ console.log(+"");
+ let num1, num2, num3;
+ num1=num2=num3=2;
+
+ let gameCounter =100
+
+ ++gameCounter;
+ gameCounter++;
+ console.log(gameCounter);
+
